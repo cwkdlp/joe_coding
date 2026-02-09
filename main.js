51de6f71774223f54eed1e0c1fa8783d0d9ab3e5
cwkdlp <cwkdlp@gmail.com>
2026-02-09 13:08:25 +0000

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const lottoNumbers = document.querySelectorAll('.number');
+
+generateBtn.addEventListener('click', () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    lottoNumbers.forEach((element, index) => {
+        element.textContent = Array.from(numbers)[index];
+    });
+});
